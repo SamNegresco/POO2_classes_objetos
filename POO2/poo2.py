eb1e297e97196f51adf8cb83c9b7eb1ce6fa7ead
Samuel Batista <samucabatista001@gmail.com>
2026-09-18 21:56:30 -0300

    Add files via upload

---
+++ POO2/poo2.py
@@ -0,0 +1,13 @@
+class Livro : 
+  def __init__(self, titulo, autor, ano):
+    self.titulo = titulo
+    self.autor = autor
+    self.ano = ano
+
+
+def editar_titulo(self, novo_titulo):
+
+
+livro1 = Livro("Dom Casmurro", "Machado de Assis", "1899")
+
+     print(f"O titulo do livro é {livro1.titulo}, e o autor é o {livro1.autor} e teve seu lancamento no ano de {livro1.ano}")
